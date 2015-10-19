003c37bda35179f6b1672626cc28cf9cf0e428ea
unknown <Khalid Abdulla@IBM-PBWTZRX.au.ibm.com>
2015-10-19 17:50:13 +1100

    futher work added to R forecasting

--- R_forecasting/demand_forecasting.R
+++ R_forecasting/demand_forecasting.R
@@ -1,4 +1,65 @@
 # Train and test 'R forecast' and plot performance
 
-source(utilityFunctions)
-allDemandData <- importDemandData()
+# ==== RUNNING OPTIONS ==== #
+nCustomers = 1
+dataFile = "../data/demand_250.csv"
+S = 48*1
+subSeries = 1:(48*100)
+lForecast = 48*2
+
+# ==== Seed for repeatability ==== #
+set.seed(42)
+
+# ==== LOAD PACKAGES ==== #
+library(forecast)
+library(ggplot2)
+
+
+# ==== LOAD FUNCTIONS ==== #
+source("utilityFunctions.R")
+
+# ==== READ IN DATA ==== #
+demandData <- read.csv(dataFile, header=FALSE)
+# produces data-frame with meters in columns, and time-steps in rows:
+nReads = dim(demandData)[1]
+nMeters = dim(demandData)[2]
+
+# ==== SELECT & SUM RANDOM SUBSET OF CUSTOMERS ==== #
+customerIndexes <- sample(1:nMeters, nCustomers, replace=F)
+if (nCustomers > 1) {
+  demandSignal_full <- rowSums(demandData[, customerIndexes])  
+} else {
+  demandSignal_full <- demandData[, customerIndexes] 
+}
+demandSignal <- demandSignal_full[subSeries]
+
+# ==== ANALYSE SERIES ==== #
+demandTS <- ts(demandSignal, frequency=S)
+plot.ts(demandTS)
+acf(demandTS, lag.max = S)
+seasonplot(demandTS)
+
+demandStar <- seasonalDiff(demandTS)
+plot.ts(demandStar)
+acf(demandStar, lag.max = S)
+
+# Force a single seasonal then first differening:
+demandStar2 <- diff(demandTS,lag=S)
+demandStar2 <- diff(demandStar2, lag=1, differences=1)
+acf(demandStar2, lag.max = S)
+
+# ==== Produce AUTO ARIMA Forecast === #
+fit <- auto.arima(demandTS)
+print(fit)
+fcast <- forecast(fit, h=lForecast)
+fcastVals <- data.frame(mean = as.numeric(fcast$mean), upper = fcast$upper[, 2],
+                        lower = fcast$lower[, 2], actual = 
+                          demandSignal_full[max(subSeries) + 1:lForecast])
+
+plot(fcast)
+
+# ==== Plot forecast point and range VS actuals === #
+print(ggplot(fcastVals, aes(x = actual, y = mean)) +
+  geom_point(size = 2) +
+  geom_errorbar(aes(ymin = lower, ymax = upper)) +
+  geom_abline(intercept=0, slope=1))
--- R_forecasting/utilityFunctions.R
+++ R_forecasting/utilityFunctions.R
@@ -15,9 +15,47 @@ importDemandData <- function() {
   
   # Find the most common no. of reads:
   commonReads <- as.numeric(names(table(as.numeric(nMeterReads))[table(as.numeric(nMeterReads)) == max(table(as.numeric(nMeterReads)))]))
-
+  
   # Select only those meters with common no. of reads:
   metersWithCommonReads <- uniqueMeters[nMeterReads == commonReads]
   
   return(demandTable)
+}
+
+# Return an appropriately differenced TS
+seasonalDiff <- function(x) {
+  ns <- nsdiffs(x)
+  if(ns > 0) {
+    xstar <- diff(x,lag=frequency(x),differences=ns)
+    print(paste0("No. seasonal differences removed: ", ns))
+  } else {
+    xstar <- x
+  }
+  nd <- ndiffs(xstar)
+  if(nd > 0) {
+    xstar <- diff(xstar,differences=nd)
+    print(paste0("No. first differences removed: ", nd))
+  }
+  return(xstar)
+}
+
+# Plot histogram with Normal approximation
+histWithNormal <- function(x) {
+  h<-hist(x, breaks=10, col="red", xlab="Value",
+          main="Histogram with Normal Curve")
+  xfit<-seq(min(x),max(x),length=40)
+  yfit<-dnorm(xfit,mean=mean(x),sd=sd(x))
+  yfit <- yfit*diff(h$mids[1:2])*length(x)
+  lines(xfit, yfit, col="blue", lwd=2) 
+}
+
+# Produce model on training data, and return RMSE on testing data:
+getRMSE <- function(x, h, ...) {
+  train.end <- time(x)[length(x)-h]
+  test.start <- time(x)[length(x)-h+1]
+  train <- window(x, end=train.end)
+  test <- window(x, start = test.start)
+  fit <- Arima(train, ...)
+  fc <- forecast(fit, h=h)
+  return(accuracy(fc, test)[2, "RMSE"])
 }
